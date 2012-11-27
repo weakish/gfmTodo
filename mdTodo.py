3b8b6fa8ba94d0f10951d1cf8c05fa0bf7675ff6
Fernando Xavier de Freitas Crespo <fernando82@gmail.com>
2012-11-27 17:39:33 -0200

    Added logic to select multiple lines
    Added logic to select multiple lines in a row and mark all as completed.
    Also updated readme.md to show the changes

--- mdTodo.py
+++ mdTodo.py
@@ -13,29 +13,33 @@ class ItodoBase(sublime_plugin.TextCommand):
 class NewCommand(ItodoBase):
   def runCommand(self, edit):
     for region in self.view.sel():
-      line = self.view.line(region)
-      # don't add a newline when creating new item with cursor is at an empty line
-      if not line:
-        line_contents = '- '
-        self.view.insert(edit, line.begin(), line_contents)
-      # add a newline when creating new item when cursor is at another line
-      else:
-        line_contents = self.view.substr(line) + '\n- '
-        self.view.replace(edit, line, line_contents)
+      lines = self.view.lines(region)
+      lines.reverse()
+      for line in lines:
+        # don't add a newline when creating new item with cursor is at an empty line
+        if not line:
+          line_contents = '- '
+          self.view.insert(edit, line.begin(), line_contents)
+        # add a newline when creating new item when cursor is at another line
+        else:
+          line_contents = self.view.substr(line) + '\n- '
+          self.view.replace(edit, line, line_contents)
 
 class CompleteCommand(ItodoBase):
   def runCommand(self, edit):    
     for region in self.view.sel():
-      line = self.view.line(region)
-      line_head = self.view.find("[-\+]", line.begin())
-      line_contents = self.view.substr(line).strip()
-      # prepend @done if item is ongoing
-      if line_contents.startswith('-'):
-        self.view.insert(edit, line.end(), " @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
-        self.view.replace(edit, line_head, "+")
-      # undo @todo
-      elif line_contents.startswith('+'):
-        subfix = self.view.find('(\s)*@done(.)+\)$', line.begin())
-        self.view.erase(edit, subfix)
-        self.view.replace(edit, line_head, "-")
+      lines = self.view.lines(region)
+      lines.reverse()
+      for line in lines:
+        line_head = self.view.find("[-\+]", line.begin())
+        line_contents = self.view.substr(line).strip()
+        # prepend @done if item is ongoing
+        if line_contents.startswith('-'):
+          self.view.insert(edit, line.end(), " @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+          self.view.replace(edit, line_head, "+")
+        # undo @todo
+        elif line_contents.startswith('+'):
+          subfix = self.view.find('(\s)*@done(.)+\)$', line.begin())
+          self.view.erase(edit, subfix)
+          self.view.replace(edit, line_head, "-")
       
